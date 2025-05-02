22d65f3281be02fa209cd65cd177e32a15867ebd
ririthewizard <ririwilliams.rw@gmail.com>
2025-05-01 22:01:00 -0500

    Brain still foggy. Will keep working at it.

--- main.go
+++ main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	cleanInput("hello world")
+}
+
+func cleanInput(text string) []string {
+	words := make([]string, len(text))
+	if len(words) == 0 {
+		return []string{}
+	}
+
+	for i := 0; i < len(text); i++ {
+		words = append(words, words[i])
+	}
+
+	fmt.Println(words)
+
+	return []string{}
 }
---
+++ repl_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Hello World   ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "    ",
+			expected: []string{},
+		},
+		{
+			input:    "HellO  WorLd ",
+			expected: []string{"hello", "world"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("lengths don't match: '%v' vs '%v'", actual, c.expected)
+			continue
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Words don't match: %v, %v", word, expectedWord)
+			}
+		}
+	}
+}
